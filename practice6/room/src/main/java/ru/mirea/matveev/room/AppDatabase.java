0cf1d4e0b2c240b39b4c93dd48c6c09df206201e
MarkFischer0 <sic3301@mail.ru>
2021-06-05 06:35:14 +0300

    practice6

---
+++ practice6/room/src/main/java/ru/mirea/matveev/room/AppDatabase.java
@@ -0,0 +1,12 @@
+package ru.mirea.matveev.room;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {MainActivity.Employee.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+    public abstract MainActivity.EmployeeDao employeeDao();
+}
